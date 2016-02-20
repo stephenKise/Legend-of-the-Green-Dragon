49d97f21c05936105b09f2f86c5092f8af85d91a
Stephen kise <stephenrkise@gmail.com>
2016-02-20 05:28:47 -0500

    DB Wrapper Type
    Now using OOP instead of procedural approach for the database.

--- lib/dbwrapper.php
+++ lib/dbwrapper.php
@@ -13,7 +13,7 @@ require_once("lib/datacache.php");
  * - mysqli_proc:		The MySQLi extension of PHP5, procedural style
  *
  */
-define('DBTYPE', 'mysqli_proc');
+define('DBTYPE', 'mysqli_oos');
 
 $dbinfo = array();
 $dbinfo['queriesthishit']=0;
