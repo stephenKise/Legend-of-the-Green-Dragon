c122c4f3d5674c7af8f2cc68645d6972e585d124
Stephen kise <stephenrkise@gmail.com>
2016-02-24 19:42:54 -0500

    Include Google API Client and Composer. Fixed an addnews() function.
    When deleting a user, the news posts properly now. Included the Google
    API Client for future use, and the composer.lock for the dependency.

--- lib/user/user_del.php
+++ lib/user/user_del.php
@@ -4,7 +4,7 @@ $res = db_query($sql);
 require_once("lib/charcleanup.php");
 char_cleanup($userid, CHAR_DELETE_MANUAL);
 while ($row = db_fetch_assoc($res)) {
-	addnews("`#%s was unmade by the gods.", $row['name'], true);
+	addnews("`#%s was unmade by the gods.", [$row['name'], true]);
 	debuglog("deleted user" . $row['name'] . "'0");
 }
 $sql = "DELETE FROM " . db_prefix("accounts") . " WHERE acctid='$userid'";
