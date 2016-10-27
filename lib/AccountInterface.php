c0feff00b4abd560957ded26831ce17ba3fb869e
Stephen Kise <stephenrkise@gmail.com>
2016-10-27 17:23:14 -0400

    Added AccountInterface interface.
    added an interface for the User class.

---
+++ lib/AccountInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace LOTGD;
+
+/**
+ * Interface to manage accounts.
+ *
+ */
+interface AccountInterface {
+    public function getID(): int;
+    public function getName(): string;
+    public function getHitpoints(): int;
+    public function getMaxHitpoints(): int;
+    public function getAttack(): int;
+    public function getDefense(): int;
+    public function getLevel(): int;
+    public function getExperience(): int;
+    public function saveUser();
+}
