50474c56cb10b3a96deda033f2129a09862bac75
Stephen Kise <stephenrkise@gmail.com>
2016-10-27 16:09:27 -0400

    Added a Log class and LoggerInterface.
    added a logging class, and a logging interface.

---
+++ lib/Log.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace LOTGD;
+
+class Log implements LoggerInterface
+{
+    protected static $database = 'gamelog';
+    public static function write(string $category, string $message, User $user)
+    {
+        $database = db_prefix(self::$database);
+        $category = addslashes($category);
+        $message = addslashes($message);
+        db_query(
+            "INSERT INTO $database (`category`, `message`, `who`)
+            VALUES ('{$category}', '{$message}', {$user->getID()})"
+        );
+    }
+    public static function read(int $logid) :array
+    {
+        $database = db_prefix(self::$database);
+        $sql = db_query("SELECT * FROM {$database} WHERE `logid` = {$logid}");
+        return db_fetch_assoc($sql);
+    }
+    public static function findByCategory(
+        string $category,
+        int $offset,
+        int $limit
+    ) :array
+    {
+        $database = db_prefix(self::$database);
+        $category = addslashes($category);
+        $sql = db_query(
+            "SELECT * FROM {$database}
+            WHERE `category` = '{$category}' LIMIT $offset, $limit"
+            );
+        debug("SELECT * FROM {$database} WHERE `category` = '{$category}'");
+        if (db_num_rows($sql) == 0) {
+            return [];
+        }
+        $returnArray = [];
+        while ($row = db_fetch_assoc($sql)) {
+            array_push($returnArray, $row);
+        }
+        return $returnArray;
+    }
+}
+
---
+++ lib/LoggerInterface.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace LOTGD;
+
+interface LoggerInterface
+{
+    public static function write(string $category, string $message, User $user);
+    public static function read(int $logid) :array;
+    public static function findByCategory(
+        string $category,
+        int $offset,
+        int $limit
+    ) :array;
+}
+
