5101daa0a5b5625d9f905d5cf518f0700cb31690
stephenKise <stephenrkise@gmail.com>
2017-01-10 12:45:24 -0500

    Removed magic quotes overrides.
    removed the overriding of magic quotes.

--- lib/errorhandling.php
+++ lib/errorhandling.php
@@ -9,26 +9,3 @@ if (defined('E_DEPRECATED')) {
 	error_reporting (E_ALL ^ E_NOTICE);
 }
 #error_reporting (E_ALL);
-
-function set_magic_quotes(&$vars) {
-	if (is_array($vars)) {
-		reset($vars);
-		while (list($key,$val) = each($vars))
-			set_magic_quotes($vars[$key]);
-	}else{
-		$vars = addslashes($vars);
-	}
-}
-
-
-//do some cleanup here to make sure magic_quotes_gpc is ON
-if (!get_magic_quotes_gpc()){
-	set_magic_quotes($_GET);
-	set_magic_quotes($_POST);
-	set_magic_quotes($_SESSION);
-	set_magic_quotes($_COOKIE);
-	set_magic_quotes($HTTP_GET_VARS);
-	set_magic_quotes($HTTP_POST_VARS);
-	set_magic_quotes($HTTP_COOKIE_VARS);
-	ini_set("magic_quotes_gpc",1);
-}
