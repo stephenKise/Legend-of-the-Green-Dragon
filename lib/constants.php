7f714be14f4e20441e5baaaa0e002b037f743265
stephenKise <sundxy@pm.me>
2023-08-14 17:43:50 -0400

    Removed the defines constant.

--- lib/constants.php
+++ lib/constants.php
@@ -1,74 +1,81 @@
 <?php
 declare(strict_types=1);
+// SuperUser flags
+define('SU_MEGAUSER', 1);
+define('SU_EDIT_MOUNTS', 2);
+define('SU_EDIT_CREATURES', 4);
+define('SU_EDIT_PETITIONS', 8);
+define('SU_EDIT_COMMENTS', 16);
+define('SU_EDIT_DONATIONS', 32);
+define('SU_EDIT_USERS', 64);
+define('SU_EDIT_CONFIG', 128);
+define('SU_INFINITE_DAYS', 256);
+define('SU_EDIT_EQUIPMENT', 512);
+define('SU_EDIT_PAYLOG', 1024);
+define('SU_DEVELOPER', 2048);
+define('SU_POST_MOTD', 4096);
+define('SU_DEBUG_OUTPUT', 8192);
+define('SU_MODERATE_CLANS', 16384);
+define('SU_EDIT_RIDDLES', 32768);
+define('SU_MANAGE_MODULES', 65536);
+define('SU_AUDIT_MODERATION', 131072);
+define('SU_IS_TRANSLATOR', 262144);
+define('SU_RAW_SQL', 524288);
+define('SU_VIEW_SOURCE', 1048576);
+define('SU_NEVER_EXPIRE', 2097152);
+define('SU_EDIT_ITEMS', 4194304);
+define('SU_GIVE_GROTTO', 8388608);
+define('SU_OVERRIDE_YOM_WARNING', 16777216);
+define('SU_SHOW_PHPNOTICE', 33554432);
+define('SU_IS_GAMEMASTER', 67108864);
+define('SU_ANYONE_CAN_SET', SU_DEBUG_OUTPUT | SU_INFINITE_DAYS | SU_OVERRIDE_YOM_WARNING | SU_SHOW_PHPNOTICE);
+define('SU_DOESNT_GIVE_GROTTO', SU_DEBUG_OUTPUT | SU_INFINITE_DAYS | SU_VIEW_SOURCE|SU_NEVER_EXPIRE);
+define('SU_HIDE_FROM_LEADERBOARD', SU_MEGAUSER | SU_EDIT_DONATIONS | SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS | SU_DEVELOPER | SU_RAW_SQL);
+define('NO_ACCOUNT_EXPIRATION', SU_HIDE_FROM_LEADERBOARD|SU_NEVER_EXPIRE);
+define('SU_GIVES_YOM_WARNING', SU_EDIT_COMMENTS | SU_EDIT_USERS | SU_EDIT_CONFIG | SU_POST_MOTD);
 
-function myDefine(string $name, $value): bool
-{
-    global $defines;
-    define($name, $value);
-    $defines[$name] = $value;
-    return true;
-}
+// Clan member rankings
+define('CLAN_APPLICANT', 0);
+define('CLAN_MEMBER', 10);
+define('CLAN_OFFICER', 20);
+define('CLAN_LEADER', 30);
+define('CLAN_FOUNDER', 31);
 
-myDefine('SU_MEGAUSER', 1);
-myDefine('SU_EDIT_MOUNTS', 2);
-myDefine('SU_EDIT_CREATURES', 4);
-myDefine('SU_EDIT_PETITIONS', 8);
-myDefine('SU_EDIT_COMMENTS', 16);
-myDefine('SU_EDIT_DONATIONS', 32);
-myDefine('SU_EDIT_USERS', 64);
-myDefine('SU_EDIT_CONFIG', 128);
-myDefine('SU_INFINITE_DAYS', 256);
-myDefine('SU_EDIT_EQUIPMENT', 512);
-myDefine('SU_EDIT_PAYLOG', 1024);
-myDefine('SU_DEVELOPER', 2048);
-myDefine('SU_POST_MOTD', 4096);
-myDefine('SU_DEBUG_OUTPUT', 8192);
-myDefine('SU_MODERATE_CLANS', 16384);
-myDefine('SU_EDIT_RIDDLES', 32768);
-myDefine('SU_MANAGE_MODULES', 65536);
-myDefine('SU_AUDIT_MODERATION', 131072);
-myDefine('SU_IS_TRANSLATOR', 262144);
-myDefine('SU_RAW_SQL', 524288);
-myDefine('SU_VIEW_SOURCE', 1048576);
-myDefine('SU_NEVER_EXPIRE', 2097152);
-myDefine('SU_EDIT_ITEMS', 4194304);
-myDefine('SU_GIVE_GROTTO', 8388608);
-myDefine('SU_OVERRIDE_YOM_WARNING', 16777216);
-myDefine('SU_SHOW_PHPNOTICE', 33554432);
-myDefine('SU_IS_GAMEMASTER', 67108864);
+// Character locations
+define('LOCATION_FIELDS', 'Degolburg');
+define('LOCATION_INN', 'The Boar\'s Head Inn');
 
-myDefine('SU_ANYONE_CAN_SET', SU_DEBUG_OUTPUT | SU_INFINITE_DAYS | SU_OVERRIDE_YOM_WARNING | SU_SHOW_PHPNOTICE);
-myDefine('SU_DOESNT_GIVE_GROTTO', SU_DEBUG_OUTPUT | SU_INFINITE_DAYS | SU_VIEW_SOURCE|SU_NEVER_EXPIRE);
-myDefine('SU_HIDE_FROM_LEADERBOARD', SU_MEGAUSER | SU_EDIT_DONATIONS | SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS | SU_DEVELOPER | SU_RAW_SQL);
-myDefine('NO_ACCOUNT_EXPIRATION', SU_HIDE_FROM_LEADERBOARD|SU_NEVER_EXPIRE);
-myDefine('SU_GIVES_YOM_WARNING', SU_EDIT_COMMENTS | SU_EDIT_USERS | SU_EDIT_CONFIG | SU_POST_MOTD);
+// Character genders
+define('SEX_MALE', 0);
+define('SEX_FEMALE', 1);
 
-myDefine('CLAN_APPLICANT', 0);
-myDefine('CLAN_MEMBER', 10);
-myDefine('CLAN_OFFICER', 20);
-myDefine('CLAN_LEADER', 30);
-myDefine('CLAN_FOUNDER', 31);
+// Character default race
+define('RACE_UNKNOWN', 'Horrible Gelatinous Blob');
+
+// Maximum integer
+define('INT_MAX', 4294967295);
 
 
-myDefine('LOCATION_FIELDS', 'Degolburg');
-myDefine('LOCATION_INN', 'The Boar\'s Head Inn');
+// Character cleanup flags
+define('CHAR_DELETE_AUTO', 1);
+define('CHAR_DELETE_MANUAL', 2);
+/**
+ * Reserved for the future; no current plans, but it seemed appropriate to have it here.
+ * @author DragonPrime Development Team?
+ * @since 1.1.2
+ */
+define('CHAR_DELETE_PERMADEATH', 3);
+define('CHAR_DELETE_SUICIDE', 4);
 
-myDefine('SEX_MALE', 0);
-myDefine('SEX_FEMALE', 1);
+// Module status flags
+define('MODULE_NO_INFO', 0);
+define('MODULE_INSTALLED', 1);
+define('MODULE_VERSION_OK', 2);
+define('MODULE_NOT_INSTALLED', 4);
+define('MODULE_FILE_NOT_PRESENT', 8);
+define('MODULE_VERSION_TOO_LOW', 16);
+define('MODULE_ACTIVE', 32);
+define('MODULE_INJECTED', 64);
 
-myDefine('INT_MAX', 4294967295);
-myDefine('RACE_UNKNOWN', 'Horrible Gelatinous Blob');
-
-myDefine('CHAR_DELETE_AUTO', 1);
-myDefine('CHAR_DELETE_MANUAL', 2);
-myDefine('CHAR_DELETE_PERMADEATH', 3); //reserved for the future -- I don't have any plans this way currently,  but it seemed appropriate to have it here. -DragonPrime Development Team?
-myDefine('CHAR_DELETE_SUICIDE', 4);
-
-myDefine('MODULE_NO_INFO', 0);
-myDefine('MODULE_INSTALLED', 1);
-myDefine('MODULE_VERSION_OK', 2);
-myDefine('MODULE_NOT_INSTALLED', 4);
-myDefine('MODULE_FILE_NOT_PRESENT', 8);
-myDefine('MODULE_VERSION_TOO_LOW', 16);
-myDefine('MODULE_ACTIVE', 32);
-myDefine('MODULE_INJECTED', 64);
+// Module directory
+define('MODULE_DIR', getcwd() . '/modules');
