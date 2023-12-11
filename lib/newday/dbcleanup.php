9c245bcf0aa92ec55b5da221f0bf1bd976bf93a1
DragonPrime <sundxy@pm.me>
2023-12-11 01:04:28 -0500

    Fix fatal error on new day

--- lib/newday/dbcleanup.php
+++ lib/newday/dbcleanup.php
@@ -1,15 +1,17 @@
 <?php
-//db cleanup
-savesetting("lastdboptimize",date("Y-m-d H:i:s"));
-$result = db_query("SHOW TABLES");
-$tables = array();
+
+require_once('lib/gamelog.php');
+savesetting('lastdboptimize', date('Y-m-d H:i:s'));
+$result = db_query('SHOW TABLES');
+$tables = [];
 $start = getmicrotime();
-for ($i=0;$i<db_num_rows($result);$i++){
+for ($i = 0; $i < db_num_rows($result); $i++) {
 	foreach (db_fetch_assoc($result) as $key => $val) {
-	db_query("OPTIMIZE TABLE $val");
-	array_push($tables,$val);
+    	db_query("OPTIMIZE TABLE $val");
+    	array_push($tables, $val);
+    }
 }
-$time = round(getmicrotime() - $start,2);
-require_once("lib/gamelog.php");
-gamelog("Optimized tables: ".join(", ",$tables)." in $time seconds.","maintenance");
+$time = round(getmicrotime() - $start, 2);
+$tableString = join(', ', $tables);
+gamelog("Optimized tables: $tableString in $time seconds.", 'maintenance');
 ?>
