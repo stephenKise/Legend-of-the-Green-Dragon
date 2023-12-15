fda0562e40a09b5847dd8f5350f4ee8c02b959fd
stephenKise <sundxy@pm.me>
2023-12-15 00:45:52 -0500

    Fix check of player mount name

--- lib/mounts.php
+++ lib/mounts.php
@@ -4,7 +4,7 @@ function getmount(int $horse = 0) {
 	if (!file_exists('dbconnect.php')) return [];
 	$mounts = db_prefix('mounts');
 	$sql = "SELECT * FROM $mounts WHERE mountid = '$horse';";
-	$result = db_query_cached($sql, "mountdata-$horse", 3600);
+	$result = db_query($sql);
 	if (db_num_rows($result) > 0) {
 		return db_fetch_assoc($result);
 	}
