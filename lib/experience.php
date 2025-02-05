9f32adfa62d40ed9a9fdbaaad5383c44ce5f4e77
stephenKise <sundxy@pm.me>
2025-02-05 04:42:30 +0000

    Reformat exp_for_next_level function

--- lib/experience.php
+++ lib/experience.php
@@ -1,30 +1,39 @@
 <?php
-// translator ready
-// addnews ready
-// mail ready
-// phpDocumentor ready
 
 /**
  * Returns the experience needed to advance to the next level.
  *
- * @param int $curlevel The current level of the player.
- * @param int $label The current number of dragonkills.
+ * @param int $currentLvl The current level of the player.
+ * @param int $currentDk Player's current dragon kills.
  * @return int The amount of experience needed to advance to the next level.
  */
-function exp_for_next_level($curlevel, $curdk)
+function exp_for_next_level(int $currentLvl, int $currentDk): int
 {
-	$exparray = array(1=>100,2=>400,3=>1002,4=>1912,5=>3140,6=>4707,
-			7=>6641,8=>8985, 9=>11795,10=>15143,11=>19121,12=>23840,
-			13=>29437,14=>36071,15=>43930);
+	if ($currentLvl < 1) return 0;
+    $expArray = [
+        1 => 100,
+        2 => 400,
+        3 => 1002,
+        4 => 1912,
+        5 => 3140,
+        6 => 4707,
+        7 => 6641,
+        8 => 8985,
+        9 => 11795,
+        10 => 15143,
+        11 => 19121,
+        12 => 23840,
+        13 => 29437,
+        14 => 36071,
+        15 => 43930
+    ];
 
-	if ($curlevel < 1) return 0;
-
-	foreach ($exparray as $key => $val) {
-		$exparray[$key] = round($val + ($curdk/4) * $key * 100, 0);
+	foreach ($expArray as $lvl => $expRequired) {
+		$expArray[$lvl] = round(
+            $expRequired + ($currentDk / 4) * $lvl * 100,
+            0
+        );
 	}
-	if ($curlevel > 15) $curlevel = 15;
-	$exprequired = $exparray[$curlevel];
-	return $exprequired;
+	if ($currentLvl > 15) $currentLvl = 15;
+	return $expArray[$currentLvl];
 }
-
-?>
