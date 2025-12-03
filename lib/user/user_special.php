6112231739e9d375ea75462fda3be3139aae3417
stephenKise <sundxy@pm.me>
2025-12-03 17:07:07 +0000

    Fix 'Give New Day' button in user editor

--- lib/user/user_special.php
+++ lib/user/user_special.php
@@ -3,7 +3,8 @@ if (httppost("newday") !=""){
 #	$offset = "-".(24 / (int)getsetting("daysperday",4))." hours";
 #	$newdate = date("Y-m-d H:i:s",strtotime($offset));
 #	$sql = "UPDATE " . db_prefix("accounts") . " SET lasthit='$newdate' WHERE acctid='$userid'";
-	$sql = "UPDATE " . db_prefix("accounts") . " SET lasthit='0000-00-00 00:00:00' WHERE acctid='$userid'";
+    $newDay = date('Y-m-d H:i:s', strtotime('-1 day'));
+	$sql = "UPDATE " . db_prefix("accounts") . " SET lasthit = '$newDay' WHERE acctid='$userid'";
 	db_query($sql);
 }elseif(httppost("fixnavs")!=""){
 	$sql = "UPDATE " . db_prefix("accounts") . " SET allowednavs='', restorepage='', specialinc='' WHERE acctid='$userid'";
