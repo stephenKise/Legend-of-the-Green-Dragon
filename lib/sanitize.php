fa82e590ea5a81d56b1cd3204599e20cfe568d77
stephenKise <sundxy@pm.me>
2025-12-03 17:01:54 +0000

    Add function to check for sanitized strings

--- lib/sanitize.php
+++ lib/sanitize.php
@@ -161,3 +161,69 @@ function htmlent(string $string): string
         getsetting('charset', 'UTF-8')
     );
 }
+
+function is_serialized($value, &$result = null): bool
+{
+	if (!is_string($value)) {
+		return false;
+	}
+
+	if ($value === 'b:0;') {
+		$result = false;
+		return true;
+	}
+
+	$length = strlen($value);
+	$end = '';
+
+	switch ($value[0]) {
+		case 's':
+			if ($value[$length - 2] !== '"') {
+				return false;
+			}
+		case 'b':
+		case 'i':
+		case 'd':
+			$end .= ';';
+		case 'a':
+		case 'O':
+			$end .= '}';
+
+			if ($value[1] !== ':') {
+				return false;
+			}
+
+			switch ($value[2]) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+				break;
+
+				default:
+					return false;
+			}
+		case 'N':
+			$end .= ';';
+
+			if ($value[$length - 1] !== $end[0]) {
+				return false;
+			}
+		break;
+
+		default:
+			return false;
+	}
+
+	if (($result = @unserialize($value)) === false) {
+		$result = null;
+		return false;
+	}
+	return true;
+}
